5e1a80684bb930f304d46ab7edbfcb5b1f157d30
danb <dan@buzzo.com>
2022-09-24 22:20:40 +0200

    added missing gif frame header file

---
+++ src/ofxGifFrame.cpp
@@ -0,0 +1,18 @@
+
+//  ofxGifFrame.h
+//  ofxGifEncoderDev
+//
+//  Created by jesus gollonet on 4/15/14.
+//
+//
+
+#pragma once
+
+typedef struct {
+    unsigned char * pixels;
+    int width;
+    int height;
+    float duration;  // seconds
+    int bitsPerPixel;
+} ofxGifFrame;
+
